6f8108706cf3b30ab85579edb601ed69802a1a05
Aryan Kumar <rahuljaikarkumar7042@gmail.com>
2025-03-11 11:57:09 +0530

    RottenOranges solutino given

--- RottenOranges.java
+++ RottenOranges.java
@@ -1,86 +1,81 @@
-
+/*
+ * leetcode: 994. Rotting Oranges
+ */
 import java.util.*;
 
-class Pair {
-
-    int i, j;
-
-    Pair() {
-
-    }
-
-    Pair(int i, int j) {
-        this.i = i;
-        this.j = j;
-    }
-}
-
 class Solution {
+
+    class Pair {
+
+        int i, j;
+
+        Pair() {
+
+        }
+
+        Pair(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+    }
 
     public int orangesRotting(int[][] grid) {
         int m = grid.length, n = grid[0].length;
 
-        Queue<Integer> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[m][n];
+        Queue<Pair> queue = new ArrayDeque<>();
 
         int freshCount = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-
                 if (grid[i][j] == 2) {
-                    queue.offer(i);
-                    queue.offer(j);
+                    queue.add(new Pair(i, j));
                 } else if (grid[i][j] == 1) {
-                    freshCount += 1;
+                    freshCount++;
                 }
-
             }
         }
-
+        System.out.println(queue.size());
+        System.out.println(freshCount);
         if (freshCount == 0) {
             return 0;
         }
 
-        int time = -1;
+        int totalTime = 0;
 
         while (!queue.isEmpty()) {
 
             int size = queue.size();
 
-            for (int index = 0; index < size; index++) {
+            boolean spread = false;
 
-                int i = queue.poll();
-                int j = queue.poll();
+            for (int idx = 0; idx < size; idx++) {
 
-                if (visited[i][j]) {
-                    continue;
-                }
-
-                grid[i][j] = 2;
-                time += 1;
-
-                visited[i][j] = true;
+                Pair p = queue.poll();
+                int i = p.i, j = p.j;
 
                 for (int di = -1; di <= 1; di++) {
                     for (int dj = -1; dj <= 1; dj++) {
-
-                        if (!(di == 0 ^ dj == 0)) {
-                            continue;
-                        }
-
-                        //todo
                         int ni = i + di;
                         int nj = j + dj;
 
-                        if (ni >= 0 && nj >= 0 && ni < m && nj < n && !visited[ni][nj]) {
-                            visited[ni][nj] = true;
+                        if ((di == 0) ^ (dj == 0) && ni >= 0 && nj >= 0 && ni < m && nj < n && grid[ni][nj] == 1)  {
+
+                            spread = true;
+                            freshCount--;
+                            grid[ni][nj] = 2;
+                            queue.offer(new Pair(ni, nj));
+
                         }
                     }
                 }
 
             }
+            if (spread) {
+                totalTime++;
+            }
         }
+        return freshCount == 0 ? totalTime : -1;
 
     }
 }
