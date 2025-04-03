ccab8303cd27b13911ff6ac8e03d0932afdd7e3f
Aryan Kumar <rahuljaikarkumar7042@gmail.com>
2025-04-03 23:39:10 +0530

    Find disjoint set with an extra info of size of respective nodes

--- DisjointSet.java
+++ DisjointSet.java
@@ -1,3 +1,31 @@
+import java.util.Arrays;
+
+public class DisjointSet {
+	public static void main(String[] args) {
+		/*
+		 * Test cases
+		 */
+		DisjointSetBySize ds = new DisjointSetBySize(7);
+
+		System.out.println(ds.findUltimateParent(3));
+
+		ds.union(1, 2);
+		ds.union(2, 3);
+		System.out.println(ds.findUltimateParent(1));
+		System.out.println(ds.findUltimateParent(3));
+		
+		ds.union(4, 5);
+		ds.union(6, 7);
+		ds.union(5, 6);
+		ds.union(3, 7);
+		
+		System.out.println(ds.findUltimateParent(1) == ds.findUltimateParent(7));
+		System.out.println(Arrays.toString(ds.size));
+		System.out.println(ds.findUltimateParent(5));
+		System.out.println(Arrays.toString(ds.size));
+	}
+}
+
 class DisjointSetByRank {
 	int parent[], rank[];
 
@@ -5,7 +33,7 @@ class DisjointSetByRank {
 		parent = new int[n + 1];
 		rank = new int[n + 1];
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i <= n; i++) {
 			parent[i] = i;
 
 		}
@@ -35,22 +63,46 @@ class DisjointSetByRank {
 		}
 	}
 
-	public static void main(String[] args) {
-		DisjointSetByRank ds = new DisjointSetByRank(7);
+}
 
-		System.out.println(ds.findUltimateParent(3));
+class DisjointSetBySize {
+	int parent[], size[];
 
-		ds.union(1, 2);
-		ds.union(2, 3);
-		System.out.println(ds.findUltimateParent(1));
-		System.out.println(ds.findUltimateParent(3));
+	DisjointSetBySize(int n) {
+		parent = new int[n + 1];
+		size = new int[n + 1];
+		Arrays.fill(size, 1);
 
-		ds.union(4, 5);
-		ds.union(6, 7);
-		ds.union(5, 6);
-		ds.union(3, 7);
-
-		System.out.println(ds.findUltimateParent(1) == ds.findUltimateParent(7));
-		System.out.println(ds.findUltimateParent(5));
+		for (int i = 0; i <= n; i++) {
+			parent[i] = i;
+		}
 	}
+
+	int findUltimateParent(int node) {
+		if (parent[node] == node)
+			return node;
+
+		int f = findUltimateParent(parent[node]);
+
+
+		return parent[node] = f; // Path compression
+	}
+
+	public void union(int u, int v) {
+		int pu = findUltimateParent(u);
+		int pv = findUltimateParent(v);
+
+		if (pu == pv)
+			return;
+
+		if (size[pu] > size[pv]) {
+			parent[pv] = pu;
+			size[pu] += size[pv];
+		} else {
+			parent[pu] = pv;
+			size[pv] += size[pu];
+		}
+
+	}
+
 }
